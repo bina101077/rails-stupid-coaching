556155c4c1d5f24ea0a2588a1b9b92d547fe4b9d
Sabina Markova <binamarkova@gmail.com>
2023-11-27 16:35:14 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+    # ... your existing routes ...
+    get '/answer', to: 'questions#answer', as: 'answer'
 
 end
